5acca946cfbdd73cb3a49c8436d82fd5d0f1bd73
Akshaj Mishra <141712076+Akshaj-mishra@users.noreply.github.com>
2026-02-09 19:27:05 +0530

    init

---
+++ Akshaj Mishra/backend/app.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(title="WebScanPro API")
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def health():
+    return {
+        "message": "FastAPI backend running 🚀"
+    }
